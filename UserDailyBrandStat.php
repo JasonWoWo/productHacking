a12187c0d7be0f5df3db894352cf565a8f3a9dbe
wangxionghao <wangxionghao@octinn.com>
2016-05-17 14:49:38 +0800

    微信绑定和关注的逻辑 写入数据库

--- UserDailyBrandStat.php
+++ UserDailyBrandStat.php
@@ -9,9 +9,9 @@
 include __DIR__ . "/middleware/UserRegisterQuery.php";
 class UserDailyBrandStat extends UserRegisterQuery
 {
-    const DAILY_PLATFORM_STAT = '';
+    const DAILY_PLATFORM_STAT = 'daily_platform_register_wechat_stat';
 
-    const DAILY_BRANDS_STAT = '';
+    const DAILY_BRANDS_STAT = 'daily_brand_register_wechat_stat';
 
     public $common;
 
@@ -29,11 +29,11 @@ class UserDailyBrandStat extends UserRegisterQuery
         $userRegisterItems = $this->getCurrentRankRegisterCnt($dateTime->getTimestamp(), 0, false);
         $brandList = $this->fetchBrandsCnt($userRegisterItems, $dateTime->getTimestamp());
         $brandList['create_on'] = "'" . $dateTime->format('Y-m-d') ."'";
-//        $insertSql = $this->common->insertParamsQuery(self::DAILY_BRANDS_STAT, $brandList);
-//        $query = $this->common->fetchCakeStatQuery($insertSql);
-//        if ($query) {
-//            echo "==== " . $brandList['create_on'] . " Register Brand Category Insert " . self::DAILY_BRANDS_STAT . " Success !!! \n";
-//        }
+        $insertSql = $this->common->insertParamsQuery(self::DAILY_BRANDS_STAT, $brandList);
+        $query = $this->common->fetchCakeStatQuery($insertSql);
+        if ($query) {
+            echo "==== " . $brandList['create_on'] . " Register Brand Category Insert " . self::DAILY_BRANDS_STAT . " Success !!! \n";
+        }
         echo "== xiaomi_cnt: " . $brandList['xiaomi_cnt'] . " == meizu_cnt: " . $brandList['meizu_cnt'] . " == huawei_cnt: " . $brandList['huawei_cnt'] .
             " == vivo_cnt: " . $brandList['vivo_cnt'] . " == samsung_cnt: " . $brandList['samsung_cnt'] . " == oppo_cnt: " . $brandList['oppo_cnt'] .
             " == zte_cnt: " . $brandList['zte_cnt'] . " \n";
@@ -46,15 +46,15 @@ class UserDailyBrandStat extends UserRegisterQuery
         $dateTime = new \DateTime(date('Y-m-d', $timeStamp));
         $dateTime->modify("-1 day");
         $userRegisterItems = $this->getCurrentRankRegisterCnt($dateTime->getTimestamp(), 0, false);
-        $params['iphone_cnt'] = $this->fetchProductListCnt($userRegisterItems, 1001, $dateTime->getTimestamp());
-        $params['android_cnt'] = $this->fetchProductListCnt($userRegisterItems, 1002, $dateTime->getTimestamp());
+        $params['iphone_register_cnt'] = $this->fetchProductListCnt($userRegisterItems, 1001, $dateTime->getTimestamp());
+        $params['android_register_cnt'] = $this->fetchProductListCnt($userRegisterItems, 1002, $dateTime->getTimestamp());
         $params['create_on'] = "'" . $dateTime->format('Y-m-d') ."'";
-        echo "=== iphone_cnt: " . $params['iphone_cnt'] . " === android_cnt: " . $params['android_cnt'] . " === create_on: " . $params['create_on'] . " \n";
-//        $insertSql = $this->common->insertParamsQuery(self::DAILY_PLATFORM_STAT, $params);
-//        $query = $this->common->fetchCakeStatQuery($insertSql);
-//        if ($query) {
-//            echo "==== " . $params['create_on'] . " Register Platform Category Insert " . self::DAILY_PLATFORM_STAT . " Success !!! \n";
-//        }
+        echo "=== iphone_register_cnt: " . $params['iphone_register_cnt'] . " === android_register_cnt: " . $params['android_register_cnt'] . " === create_on: " . $params['create_on'] . " \n";
+        $insertSql = $this->common->insertParamsQuery(self::DAILY_PLATFORM_STAT, $params);
+        $query = $this->common->fetchCakeStatQuery($insertSql);
+        if ($query) {
+            echo "==== " . $params['create_on'] . " Register Platform Category Insert " . self::DAILY_PLATFORM_STAT . " Success !!! \n";
+        }
     }
 }
 $userBrandList = new UserDailyBrandStat();
--- WeChatAuthorize.php
+++ WeChatAuthorize.php
@@ -9,9 +9,9 @@
 include __DIR__ . "/middleware/RegisterWechatAuthorize.php";
 class WeChatAuthorize extends RegisterWechatAuthorize
 {
-    const DAILY_PLATFORM_STAT = '';
+    const DAILY_PLATFORM_STAT = 'daily_platform_register_wechat_stat';
 
-    const DAILY_BRANDS_STAT = '';
+    const DAILY_BRANDS_STAT = 'daily_brand_register_wechat_stat';
 
     public $common;
 
@@ -23,38 +23,36 @@ class WeChatAuthorize extends RegisterWechatAuthorize
 
     public function updatePlatformRelateRatio()
     {
-//        $paramsId = array('id');
-//        $current = new \DateTime();
-//        $where = array('create_on' => "'" . $current->modify('-1 day')->format('Y-m-d') ."'");
-//        $selectQuery = $this->common->selectParamsQuery(self::DAILY_PLATFORM_STAT, $paramsId, $where);
-//        $result = $this->common->fetchAssoc($selectQuery);
-//        if (empty($result)) {
-//            echo "UnCatch the result where create_on :" . $where['create_on'] . "In " . self::DAILY_PLATFORM_STAT . " \n";
-//            return ;
-//        }
+        $paramsId = array('id');
+        $current = new \DateTime();
+        $where = array('create_on' => "'" . $current->modify('-1 day')->format('Y-m-d') ."'");
+        $selectQuery = $this->common->selectParamsQuery(self::DAILY_PLATFORM_STAT, $paramsId, $where);
+        $result = $this->common->fetchAssoc($selectQuery);
+        if (empty($result)) {
+            echo "UnCatch the result where create_on :" . $where['create_on'] . "In " . self::DAILY_PLATFORM_STAT . " \n";
+            return ;
+        }
         $paramsIphone = $this->fetchProductListCnt(1001);
         $paramsAndroid = $this->fetchProductListCnt(1002);
         $params = $paramsIphone + $paramsAndroid;
-        var_dump($params);
-        echo  '------------------';
-//        $updateQuery = $this->common->updateParamsQuery(self::DAILY_PLATFORM_STAT, $params, $where);
-//        $query = $this->common->fetchCakeStatQuery($updateQuery);
-//        if ($query) {
-//            echo " ===" . $where['create_on'] ." Update " . self::DAILY_PLATFORM_STAT . " success !!! \n";
-//        }
+        $updateQuery = $this->common->updateParamsQuery(self::DAILY_PLATFORM_STAT, $params, $where);
+        $query = $this->common->fetchCakeStatQuery($updateQuery);
+        if ($query) {
+            echo " ===" . $where['create_on'] ." Update " . self::DAILY_PLATFORM_STAT . " success !!! \n";
+        }
     }
 
     public function updateBrandRelateRatio()
     {
-//        $paramsId = array('id');
-//        $current = new \DateTime();
-//        $where = array('create_on' => "'" . $current->modify('-1 day')->format('Y-m-d') ."'");
-//        $selectQuery = $this->common->selectParamsQuery(self::DAILY_BRANDS_STAT, $paramsId, $where);
-//        $result = $this->common->fetchAssoc($selectQuery);
-//        if (empty($result)) {
-//            echo "UnCatch the result where create_on :" . $where['create_on'] . "In " . self::DAILY_BRANDS_STAT . " \n";
-//            return ;
-//        }
+        $paramsId = array('id');
+        $current = new \DateTime();
+        $where = array('create_on' => "'" . $current->modify('-1 day')->format('Y-m-d') ."'");
+        $selectQuery = $this->common->selectParamsQuery(self::DAILY_BRANDS_STAT, $paramsId, $where);
+        $result = $this->common->fetchAssoc($selectQuery);
+        if (empty($result)) {
+            echo "UnCatch the result where create_on :" . $where['create_on'] . "In " . self::DAILY_BRANDS_STAT . " \n";
+            return ;
+        }
         // 先注解调,看数据
         $brandItems = $this->getRegisterMappingBrandInit();
         $brandBindItems = $this->fetchBrandsBindRelateRatio();
@@ -74,30 +72,30 @@ class WeChatAuthorize extends RegisterWechatAuthorize
         $brandItems['vivo_bind_focus_mp_cnt'] = $brandBindFocusItems['vivo_cnt'];
         $brandItems['samsung_bind_focus_mp_cnt'] = $brandBindFocusItems['samsung_cnt'];
         $brandItems['zte_bind_focus_mp_cnt'] = $brandBindFocusItems['zte_cnt'];
-        echo " ==== xiaomi_bind_mp_cnt: " . $brandItems['xiaomi_bind_mp_cnt'] . " \n";
-        echo " ==== meizu_bind_mp_cnt: " . $brandItems['meizu_bind_mp_cnt'] . " \n";
-        echo " ==== oppo_bind_mp_cnt: " . $brandItems['oppo_bind_mp_cnt'] . " \n";
-        echo " ==== huawei_bind_mp_cnt: " . $brandItems['huawei_bind_mp_cnt'] . " \n";
-        echo " ==== vivo_bind_mp_cnt: " . $brandItems['vivo_bind_mp_cnt'] . " \n";
-        echo " ==== samsumg_bind_mp_cnt: " . $brandItems['samsung_bind_mp_cnt'] . " \n";
-        echo " ==== zte_bind_mp_cnt: " . $brandItems['zte_bind_mp_cnt'] . " \n";
-        echo " ==== xiaomi_f_bind_mp_cnt: " . $brandItems['xiaomi_bind_focus_mp_cnt'] . " \n";
-        echo " ==== meizu_f_bind_mp_cnt: " . $brandItems['meizu_bind_focus_mp_cnt'] . " \n";
-        echo " ==== oppp_f_bind_mp_cnt: " . $brandItems['oppo_bind_focus_mp_cnt'] . " \n";
-        echo " ==== huawei_f_bind_mp_cnt: " . $brandItems['huawei_bind_focus_mp_cnt'] . " \n";
-        echo " ==== vivo_f_bind_mp_cnt: " . $brandItems['vivo_bind_focus_mp_cnt'] . " \n";
-        echo " ==== samsung_f_bind_mp_cnt: " . $brandItems['samsung_bind_focus_mp_cnt'] . " \n";
-        echo " ==== zte_f_bind_mp_cnt: " . $brandItems['zte_bind_focus_mp_cnt'] . " \n";
+//        echo " ==== xiaomi_bind_mp_cnt: " . $brandItems['xiaomi_bind_mp_cnt'] . " \n";
+//        echo " ==== meizu_bind_mp_cnt: " . $brandItems['meizu_bind_mp_cnt'] . " \n";
+//        echo " ==== oppo_bind_mp_cnt: " . $brandItems['oppo_bind_mp_cnt'] . " \n";
+//        echo " ==== huawei_bind_mp_cnt: " . $brandItems['huawei_bind_mp_cnt'] . " \n";
+//        echo " ==== vivo_bind_mp_cnt: " . $brandItems['vivo_bind_mp_cnt'] . " \n";
+//        echo " ==== samsumg_bind_mp_cnt: " . $brandItems['samsung_bind_mp_cnt'] . " \n";
+//        echo " ==== zte_bind_mp_cnt: " . $brandItems['zte_bind_mp_cnt'] . " \n";
+//        echo " ==== xiaomi_f_bind_mp_cnt: " . $brandItems['xiaomi_bind_focus_mp_cnt'] . " \n";
+//        echo " ==== meizu_f_bind_mp_cnt: " . $brandItems['meizu_bind_focus_mp_cnt'] . " \n";
+//        echo " ==== oppp_f_bind_mp_cnt: " . $brandItems['oppo_bind_focus_mp_cnt'] . " \n";
+//        echo " ==== huawei_f_bind_mp_cnt: " . $brandItems['huawei_bind_focus_mp_cnt'] . " \n";
+//        echo " ==== vivo_f_bind_mp_cnt: " . $brandItems['vivo_bind_focus_mp_cnt'] . " \n";
+//        echo " ==== samsung_f_bind_mp_cnt: " . $brandItems['samsung_bind_focus_mp_cnt'] . " \n";
+//        echo " ==== zte_f_bind_mp_cnt: " . $brandItems['zte_bind_focus_mp_cnt'] . " \n";
 
         // 先注解调,看数据
-//        $updateQuery = $this->common->updateParamsQuery(self::DAILY_BRANDS_STAT, $brandItems, $where);
-//        $query = $this->common->fetchCakeStatQuery($updateQuery);
-//        if ($query) {
-//            echo " ===" . $where['create_on'] ." Update " . self::DAILY_BRANDS_STAT . " success !!! \n";
-//        }
+        $updateQuery = $this->common->updateParamsQuery(self::DAILY_BRANDS_STAT, $brandItems, $where);
+        $query = $this->common->fetchCakeStatQuery($updateQuery);
+        if ($query) {
+            echo " ===" . $where['create_on'] ." Update " . self::DAILY_BRANDS_STAT . " success !!! \n";
+        }
 
     }
 }
 $weChatAuth = new WeChatAuthorize();
 $weChatAuth->updateBrandRelateRatio();
-//$weChatAuth->updatePlatformRelateRatio();
+$weChatAuth->updatePlatformRelateRatio();
