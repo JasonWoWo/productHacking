ef648dc0bb2ec08bfdac826c9d7d29258f5f33ab
wangxionghao <wangxionghao@octinn.com>
2016-07-08 14:39:14 +0800

    fix取数逻辑

--- UtilSpace/UtilSqlTool.php
+++ UtilSpace/UtilSqlTool.php
@@ -288,7 +288,7 @@ trait UtilSqlTool
         }
         $query = "SELECT u.id, u.udid, DATE_FORMAT(u.create_on, '%Y-%m-%d') AS create_on, DATE_FORMAT(u.visit_on, '%Y-%m-%d') AS visit_on, u.appid, u.chnid FROM oibirthday.users AS u WHERE u.id > 5000000 ";
         if ($createOnStartTimeStamp) {
-            $query .= " AND TO_DAYS(u.create_on) >= {$createOn} AND TO_DAYS(u.create_on) <= {$createOnEnd}";
+            $query .= " AND TO_DAYS(u.create_on) >= {$createOnEnd} AND TO_DAYS(u.create_on) <= {$createOn}";
         }
         return $query;
     }
